627eb8ef4b6350b5bb00bda4558eb6a056d6c9e1
00lar <eduardani84@gmail.com>
2023-11-22 02:08:28 -0700

    make endpoints with get

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+
 app.get("/",(req, res) => {
   res.send("hello to my server in express")
 } )
@@ -11,11 +12,38 @@ res.send("Nuevo endpoint")
 })
 
 app.get("/products",(req,res) => {
-  res.json ({
-    name: "product 1",
-    price:1000,
+  res.json ([
+    {
+      name: "product 1",
+      price:1000,
+    },
+    {
+      name: "product 2",
+      price: 1000,
+    }
+
+  ])
+})
+
+app.get("/products/:id", (req,res) => {
+  const {id} = req.params;
+  res.json({
+    id,
+    name: "product 2",
+      price: 1000,
+  })
+}),
+
+app.get("/categories/:categoryId/products/:productId", (req, res) => {
+  const {categoryId, productId} = req.params;
+  res.json({
+    id,
+    categoryId,
+    productId
   })
 })
+
+
 
 app.listen(port, ()=> {
   console.log("My port:" + port )
