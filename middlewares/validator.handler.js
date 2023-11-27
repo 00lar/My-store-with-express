d798cea950842e6ae864ce4d652f88a7c80044a1
00lar <eduardani84@gmail.com>
2023-11-26 22:09:02 -0700

    trying the endpoints

--- middlewares/validator.handler.js
+++ middlewares/validator.handler.js
@@ -3,10 +3,13 @@ const boom = require('@hapi/boom');
 function validatorHandler(schema, property) {
   return (req, res, next) => {
     const data = req[property];
-    const { error } = schema.validate(data);
+    //el abort es para mandar varios errores alv
+    const { error } = schema.validate(data, {abortEarly: false});
     if (error) {
       next(boom.badRequest(error));
     }
     next();
   };
 }
+
+module.exports = validatorHandler
